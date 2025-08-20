aa8cf6770d72951a0465db129159196317211225
mike-droid <miguelreyesmoreno@hotmail.com>
2025-08-19 22:14:37 -0500

    agregando middlewares

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+const LoggerMiddleware = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(LoggerMiddleware);
+app.use(errorHandler);
 app.use('/api', routes);
 
 app.get('/', (req, res) => {
